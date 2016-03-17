5cbdfed8f850e0dd74aed35306c825bf5211bca1
Daniel Dimitrov <dimitrov.dan@gmail.com>
2016-03-17 17:16:15 +0100

    Finish Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,52 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
     @project = Project.new
   end
+
+  def edit
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @project.update_attributes(project_params)
+      redirect_to projects_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+
+    redirect_to projects_path
+  end
+
+  private
+
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+    def project_params
+      params.require(:project).permit(:name, :description)
+    end
 
 end
